7990f383c1e7b63fdfe4c6c2bb92044d7a1bd768
BBhushan1 <146929444+BBhushan1@users.noreply.github.com>
2024-05-27 16:15:13 +0530

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function myMenuFunction(){
   }
 }
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
+  const menu = document.querySelector(".menu-links"); 
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
